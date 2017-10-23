814e194934e3619d988bd0560c374404a1b30d82
kofiav <victor.kofia@gmail.com>
2017-10-23 09:46:42 -0400

    Make docs ROxygen compatible

--- R/CREAM.R
+++ R/CREAM.R
@@ -1,13 +1,21 @@
-
-############################# CREAM is the main function for CORE identification
-############################# Input variables of this function are as follows:
-############################# 1) in_path: Path to the input file (The file inclusing the functional regions)
-############################# Note. You have to make sure that there is no overlapping regions within the input file
-############################# 2) out_path: the path in which you want to store the identified COREs
-############################# 3) MinLength: Criteria for the minimum number of functional regions in the input file
-############################# 4) peakNumMin: Minimum number of peaks for CORE identification
-############################# 5) WScutoff: Threshold used to identify WS within distribution of maximum distance between peaks for each Order of CORE
-
+#' CREAM is the main function for CORE identification
+#'
+#' @param in_path Path to the input file (The file inclusing the functional
+#' regions)
+#' Note. You have to make sure that there is no overlapping regions within the
+#' input file
+#' @param out_path The path in which you want to store the identified COREs
+#' @param MinLength Criteria for the minimum number of functional regions in the
+#'  input file
+#' @param peakNumMin Minimum number of peaks for CORE identification
+#' @param WScutoff Threshold used to identify WS within distribution of maximum distance between peaks for each order of CORE
+#' @return Bed file including the identified COREs
+#' @examples \dontrun{
+#' CREAM("A549_Chr21.bed", "A549_Chr21_COREs.bed", MinLength = 1000,
+#' peakNumMin = 2)
+#' }
+#' @importFrom utils read.table write.table
+#' @export
 CREAM <- function(in_path, out_path, WScutoff = 1.5, MinLength = 1000, peakNumMin = 2){
   InputData   <- read.table(in_path, sep="\t")
   colnames(InputData) <- c("chr", "start", "end")
@@ -28,20 +36,20 @@ CREAM <- function(in_path, out_path, WScutoff = 1.5, MinLength = 1000, peakNumMi
   ####################
   if(!is.null(StartSeq_Vec)){
     CombinedData <- cbind(ChrSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]],
-                          StartSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]], 
+                          StartSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]],
                           EndSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]],
-                          OrderSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]], 
+                          OrderSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]],
                           WidthSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]],
                           WinSizeSeq_Vec[sort(OrderSeq_Vec, decreasing = T, index.return = T)[[2]]])
     colnames(CombinedData) <- c("Chr", "Start", "End", "Order", "Width", "WindowSize")
-    
+
     MinPeaks <- PeakMinFilt(CombinedData, WindowVecFinal)
-    
+
     RemovePeaks <- which(as.numeric(CombinedData[,"Order"]) < MinPeaks)
     if(length(RemovePeaks) > 0){
       CombinedData <- CombinedData[-RemovePeaks,]
     }
-    
+
     CombinedData <- CombinedData[,c(1:3)]
     colnames(CombinedData) <- NULL
     write.table(CombinedData , file = out_path,
